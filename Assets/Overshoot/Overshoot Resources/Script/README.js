3cae83c61c9f637e932904cbaac1c2d474c4f994
GlitteringWorld <akilahmartinez@gmail.com>
2024-11-10 03:33:36 -0700

    2:33AM
    Yep

---
+++ Assets/Overshoot/Overshoot Resources/Script/README.js
@@ -0,0 +1,19 @@
+/*
+
+README: Overshoot
+=============================================
+
+This asset is represented by 2d and 3d examples of using the overshoot: 
+    - Animation in the 2D example is controlled by the "Overshoot" script, which is designed only for screen images
+    - Animation in 3D example is controlled by "Overshoot" subgraph inside ducky material.
+
+Please ASSIGN YOUR MAIN RENDER TARGET to the orthographic camera in the 2D example to make it visible.
+
+Overshoot parameters:
+    Frequency - how bouncy the animation will be
+    Amplitude - how strong the overshoot will be
+    Duration - the speed of the animation transition from the start value to the end value
+    Start Value - the initial value of the animation
+    End Value - the final value of the animation
+    Decay - how fast the oscillations decay
+*/
